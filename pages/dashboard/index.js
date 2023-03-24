088ddd6226afa0351afe946f8e04a9a67b006667
requiemcreatif <requiemcreatif@gmail.com>
2023-03-24 10:51:30 +0100

    build

--- pages/dashboard/index.js
+++ pages/dashboard/index.js
@@ -1,8 +1,28 @@
+import { useState } from "react";
 import { connectToDatabase } from "../../util/mongodb";
 import UserList from "../../components/UserList";
 import Header from "@/components/Header";
 
+// export default function Dashboard({ users }) {
+//   return (
+//     <div>
+//       <Header />
+//       <div className=" p-5 mx-auto flex justify-center">
+//         <input
+//           className=" outline-none h-20 py-2 px-10 rounded-full shadow-lg w-full lg:w-3/5 border"
+//           type="text"
+//         />
+//       </div>
+
+//       <div>
+//         <UserList users={users} />
+//       </div>
+//     </div>
+//   );
+// }
+
 export default function Dashboard({ users }) {
+  const [user, setUser] = useState(users);
   return (
     <div>
       <Header />
@@ -12,22 +32,24 @@ export default function Dashboard({ users }) {
           type="text"
         />
       </div>
-
-      <div>
-        <UserList users={users} />
+      <div className=" grid grid-cols-1 gap-10 lg:grid-cols-3 p-5 lg:max-w-[1500px] mx-auto mt-20">
+        {user.map((user) => (
+          <div key={user._id}>
+            <UserList user={user} />
+          </div>
+        ))}
       </div>
     </div>
   );
 }
 
-export async function getServerSideProps(context) {
+export const getServerSideProps = async (context) => {
   const db = await connectToDatabase();
-  const collection = await db.collection("users");
-  const users = await collection.find({}).toArray();
-
+  const data = await db.collection("users").find({}).toArray();
+  const users = JSON.parse(JSON.stringify(data));
   return {
     props: {
-      users: JSON.parse(JSON.stringify(users)),
+      users: users,
     },
   };
-}
+};
--- util/mongodb.js
+++ util/mongodb.js
@@ -6,3 +6,43 @@ export async function connectToDatabase() {
   await client.connect();
   return client.db(process.env.MONGO_DB);
 }
+
+// import { MongoClient } from "mongodb";
+
+// const { MONGO_URI, MONGO_DB } = process.env;
+
+// if (!MONGO_URI) {
+//   throw new Error("Please define the MONGO_URI environment variable inside .env.local");
+// }
+
+// if (!MONGO_DB) {
+//   throw new Error("Please define the MONGO_DB environment variable inside .env.local");
+// }
+
+// let cached = global.mongo;
+
+// if (!cached) {
+//   cached = global.mongo = { conn: null, promise: null };
+// }
+
+// export async function connectToDatabase() {
+//   if (cached.conn) {
+//     return cached.conn;
+//   }
+
+//   if (!cached.promise) {
+//     const opts = {
+//       useNewUrlParser: true,
+//       useUnifiedTopology: true,
+//     };
+
+//     cached.promise = MongoClient.connect(MONGO_URI, opts).then((client) => {
+//       return {
+//         client,
+//         db: client.db(MONGO_DB),
+//       };
+//     });
+//   }
+//   cached.conn = await cached.promise;
+//   return cached.conn;
+// }
