de77e4b20a958e23d8863d18e20dab7142f1a416
requiemcreatif <requiemcreatif@gmail.com>
2023-04-06 21:34:02 +0200

    background styling

--- components/MainHeader.js
+++ components/MainHeader.js
@@ -1,14 +1,14 @@
 import Image from "next/image";
-import Logo from "../images/logo-black.svg";
+import Logo from "../images/studio-g-white.svg";
 
 const MainHeader = () => {
   return (
-    <div className="shadow-md bg-white fixed w-full">
+    <div className=" top-0 fixed w-full bg-[transparent]">
       <div className="  p-5 flex justify-between max-w-[1200px] mx-auto">
         <nav className=" ">
           <Image src={Logo} alt="Logo Studio G" width={45} height={50} />
         </nav>
-        <button className="  bg-[#000000] rounded-md px-5 h-16 w-52 font-semibold text-white">
+        <button className="  bg-[#ffffff] rounded-md px-5 h-16 w-52 font-semibold text-blackl">
           Login
         </button>
       </div>
--- pages/index.js
+++ pages/index.js
@@ -5,35 +5,39 @@ import MainHeader from "@/components/MainHeader";
 
 export default function Home() {
   return (
-    <div className=" bg-white">
+    <div className=" h-screen">
       <MainHeader />
-      <main className=" p-10 h-screen">
-        <div className=" lg:px-40 grid grid-cols-1 max-w-[1200px] gap-16 mx-auto mt-60 bg-[#000000] rounded-[25px] p-10 ">
-          <div className="grid grid-cols-1 max-w-7xl">
-            <div className=" ">
-              <h1 className="text-white text-7xl lg:text-9xl font-bold">CENTRO DEPORTIVO</h1>
+      <main className=" lg:py-10 h-[auto] bg-gradient-to-r from-gray-700 via-gray-900 to-black  lg:h-screen ">
+        <section className=" pt-20">
+          <div className=" lg:px-0 grid grid-cols-1 max-w-[1200px] gap-16 mx-auto rounded-[25px] py-20 px-10">
+            <div className="grid grid-cols-1 max-w-7xl">
+              <div className=" ">
+                <h1 className="text-white text-7xl lg:text-9xl font-bold">CENTRO DEPORTIVO</h1>
+              </div>
+            </div>
+            <div className=" grid grid-cols-1 max-w-7xl gap-16">
+              <p className="text-white text-left">
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quia doloremque sint
+                adipisci dolorum dolores nobis placeat a quas id itaque, obcaecati optio praesentium
+                culpa cupiditate voluptate. In est ducimus eius. Lorem, ipsum dolor sit amet
+                consectetur adipisicing elit.
+              </p>
+              <div>
+                <Link href="/register">
+                  <button
+                    className=" hover:scale-105 cursor-pointer transition-all duration-300 
+                  bg-gradient-to-b from-red-500 to-red-800
+                   text-white rounded-full px-5 h-[40px] w-52 font-semibold">
+                    Registrar
+                  </button>
+                </Link>
+              </div>
             </div>
           </div>
-          <div className=" grid grid-cols-1 max-w-7xl gap-16">
-            <p className="text-white text-left">
-              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quia doloremque sint
-              adipisci dolorum dolores nobis placeat a quas id itaque, obcaecati optio praesentium
-              culpa cupiditate voluptate. In est ducimus eius. Lorem, ipsum dolor sit amet
-              consectetur adipisicing elit. Quia doloremque sint adipisci dolorum dolores nobis
-              placeat a quas id itaque, obcaecati optio praesentium culpa cupiditate voluptate. In
-              est ducimus eius.
-            </p>
-            <div>
-              <Link href="/register">
-                <button className=" bg-red-700 text-white rounded-full px-5 py-3 h-20 w-52 font-semibold">
-                  Registrar
-                </button>
-              </Link>
-            </div>
-          </div>
-        </div>
-        <section className=" grid lg:grid-cols-3 gap-5 max-w-[1200px] mx-auto mt-20">
-          <div className="shadow-md transition duration-300 rounded-[25px] p-10 h-80 grid grid-rows-[40%_60%] bg-white">
+        </section>
+
+        <section className=" px-10 py-20 grid lg:px-1 lg:grid-cols-3 gap-10 max-w-[1200px] mx-auto ">
+          <div className="shadow-[0_0_5px_rgba(0,0,0,0.3)] hover:scale-105 hover:rounded-none cursor-pointer transition-all duration-500 rounded-[15px] p-10 h-80 grid grid-rows-[40%_60%] bg-white">
             <div>
               <h2 className="font-semibold text-3xl uppercase">Entrenamiento Personal</h2>
             </div>
@@ -44,7 +48,8 @@ export default function Home() {
               </p>
             </div>
           </div>
-          <div className=" shadow-md transition duration-300 bg-white rounded-[25px] p-10 h-80 grid grid-rows-[40%_60%]">
+
+          <div className="shadow-[0_0_5px_rgba(0,0,0,0.3)] hover:scale-105 hover:rounded-none cursor-pointer transition-all duration-500 rounded-[15px] p-10 h-80 grid grid-rows-[40%_60%] bg-white">
             <div>
               <h2 className="font-semibold text-3xl uppercase">Entrenamiento Funcional</h2>
             </div>
@@ -55,7 +60,7 @@ export default function Home() {
               </p>
             </div>
           </div>
-          <div className="shadow-md transition duration-300 bg-white rounded-[25px] p-10 h-80 grid grid-rows-[40%_60%]">
+          <div className="shadow-[0_0_5px_rgba(0,0,0,0.3)] hover:scale-105 hover:rounded-none cursor-pointer transition-all duration-500 rounded-[15px] p-10 h-80 grid grid-rows-[40%_60%] bg-white">
             <div>
               <h2 className="font-semibold text-3xl uppercase">Readaptacion y recuperacion</h2>
             </div>
